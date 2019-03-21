a8a4aaa4f0ba828e2db9217ebfa9f618efe6b625
jerlogan <jeremy.logan.web@gmail.com>
2019-03-21 10:07:08 -0500

    exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,4 @@
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
@@ -12,17 +12,77 @@ public class ControlFlowExercises {
         for (int x = 100; x > -11; x-=5) {
             System.out.println(x);
         }
-        for (int w = 2; w < 1000000; w++) {
-            int v = w * w;
 
-            System.out.println(v * v);
-            if (v >= 1000000) {
-                break;
-            }
+
+
+
+        for (int y = 2; y <= 65535; y *= y  ) {
+            System.out.println(y);
         }
 
 
 
 
+
+        for (int s = 1; s <= 100; s++) {
+            if (s % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (s % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else if (s % 15 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else {
+                System.out.println(s);
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer:");
+        String userString = scanner.next();
+        System.out.println("Great! You have entered " + userString);
+
+        int userNumber = Integer.parseInt(userString);
+
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int i = 1; i <= userNumber; i++) {
+
+            int numSquared = i * i;
+            int numCubed = i * i * i;
+
+            System.out.println(i + "      |  " + numSquared + "      |  " + numCubed + "     ");
+        }
+
+
+        System.out.println("What is little Jimmy's grade (from 0 to 100)?");
+        String userString2 = scanner.next();
+        int userNumber2 =  Integer.parseInt(userString2);
+        char grade;
+        if (userNumber2 >= 88) {
+            grade = 'A';
+            System.out.println(grade);
+        }
+        if (userNumber2 >= 80 && userNumber <= 87) {
+            grade = 'B';
+            System.out.println(grade);
+        }
+        if (userNumber2 >= 67 && userNumber <= 79) {
+            grade = 'C';
+            System.out.println(grade);
+        }
+        if (userNumber2 >= 60 && userNumber <= 66) {
+            grade = 'D';
+            System.out.println(grade);
+        }
+        else if (userNumber2 >= 0 && userNumber <= 59) {
+            grade = 'F';
+            System.out.println(grade);
+        }
+
+        System.out.println("Hit enter to continue.");
     }
 }
